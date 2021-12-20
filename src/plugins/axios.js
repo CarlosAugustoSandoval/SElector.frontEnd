fc0ac34a81ec2fa26119485b31869bed2c51f77c
krlit <krlitozagusto@hotmail.es>
2021-12-20 15:45:44 -0500

    componente de card personalizada. componente de confirmación personalizado. datos de user y fechas en registro de persona. inicio módulo de usuarios. borrado de personas.

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 // axios.defaults.baseURL = 'http://localhost:8000/api'
-axios.defaults.baseURL = 'http://179.50.78.197/api'
+axios.defaults.baseURL = 'http://179.50.78.197:8000/api'
 Vue.use(VueAxios, axios)
