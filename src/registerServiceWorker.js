ee93ea6001d720978996ec57314ed9f89a2899f7
krlit <krlitozagusto@hotmail.es>
2022-01-03 23:54:06 -0500

    *logs en serviceWorker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -22,7 +22,6 @@ if (process.env.NODE_ENV === 'production') {
     updated (registration) {
       console.log('New content is available; please refresh.', registration)
       let worker = registration.waiting
-      console.log('worker.', worker)
       if (worker) worker.postMessage({action: 'skipWaiting'})
     },
     offline () {
