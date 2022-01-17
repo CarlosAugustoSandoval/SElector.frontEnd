faaa4e14e8a21ff3a14284aa25e436be13541c13
krlit <krlitozagusto@hotmail.es>
2022-01-17 17:08:44 -0500

    prueba de scanX

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="page-wrapper">
     <div>
-      Home 7
+      Home 8
     </div>
     <div>b{{error}}</div>
     <div>c{{controls}}</div>
@@ -13,16 +13,8 @@
 </template>
 
 <script>
-// import {BrowserPDF417Reader} from '@zxing/browser'
-// import * as ZXingBrowser from "@zxing/browser";
-import {
-  MultiFormatReader,
-  BarcodeFormat,
-  DecodeHintType,
-  RGBLuminanceSource,
-  BinaryBitmap,
-  HybridBinarizer
-} from '@zxing/library'
+import {BrowserCodeReader} from '@zxing/browser'
+import * as ZXingBrowser from "@zxing/browser";
 
 export default {
   name: 'Home',
@@ -34,44 +26,31 @@ export default {
   }),
   async mounted() {
     if (this.user?.id === 1) {
-      const hints = new Map();
-      const formats = [BarcodeFormat.QR_CODE, BarcodeFormat.PDF_417/*, ...*/];
+      const codeReader = new BrowserCodeReader()
+      const videoInputDevices = await ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
 
-      hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+// choose your media device (webcam, frontal camera, back camera, etc.)
+      const selectedDeviceId = videoInputDevices[1].deviceId;
 
-      const reader = new MultiFormatReader();
+      console.log(`Started decode from camera with id ${selectedDeviceId}`);
 
-      reader.setHints(hints);
+      const previewElem = document.querySelector('#test-area-qr-code-webcam > video');
 
-      const luminanceSource = new RGBLuminanceSource(null, 500, 500);
-      const binaryBitmap = new BinaryBitmap(new HybridBinarizer(luminanceSource));
+// you can use the controls to stop() the scan or switchTorch() if available
+      const controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem, (result, error, controls) => {
+        this.result = result
+        this.error = error
+        this.controls = controls
+        console.log('result', result)
+        console.log('error', error)
+        console.log('controls', controls)
+        // use the result and error values to choose your actions
+        // you can also use controls API in this scope like the controls
+        // returned from the method.
+      });
 
-      reader.decode(binaryBitmap);
-//       const codeReader = new BrowserPDF417Reader()
-//       const videoInputDevices = await ZXingBrowser.BrowserCodeReader.listVideoInputDevices();
-//
-// // choose your media device (webcam, frontal camera, back camera, etc.)
-//       const selectedDeviceId = videoInputDevices[1].deviceId;
-//
-//       console.log(`Started decode from camera with id ${selectedDeviceId}`);
-//
-//       const previewElem = document.querySelector('#test-area-qr-code-webcam > video');
-//
-// // you can use the controls to stop() the scan or switchTorch() if available
-//       const controls = await codeReader.decodeFromVideoDevice(selectedDeviceId, previewElem, (result, error, controls) => {
-//         this.result = result
-//         this.error = error
-//         this.controls = controls
-//         console.log('result', result)
-//         console.log('error', error)
-//         console.log('controls', controls)
-//         // use the result and error values to choose your actions
-//         // you can also use controls API in this scope like the controls
-//         // returned from the method.
-//       });
-//
-// // stops scanning after 20 seconds
-//       setTimeout(() => controls.stop(), 50000)
+// stops scanning after 20 seconds
+      setTimeout(() => controls.stop(), 50000)
     }
   }
 }
